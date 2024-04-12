fc39ed10d93138333b492881f8fc23de8b9d5268
Werapat Wangrungroj <94524977+hellp002@users.noreply.github.com>
2024-04-12 20:32:42 +0700

    Delete cpsysinfo/.cpsysinfo.mod.cmd

--- cpsysinfo/.cpsysinfo.mod.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/ubuntu/hw12/cpsysinfo/cpsysinfo.mod := printf '%s\n'   cpsysinfo.o | awk '!x[$$0]++ { print("/home/ubuntu/hw12/cpsysinfo/"$$0) }' > /home/ubuntu/hw12/cpsysinfo/cpsysinfo.mod
