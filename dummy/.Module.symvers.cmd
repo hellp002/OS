7d3130b51314095dbd7cad64f7284cc70b7afae7
Werapat Wangrungroj <94524977+hellp002@users.noreply.github.com>
2024-04-12 20:35:35 +0700

    Delete dummy/.Module.symvers.cmd

--- dummy/.Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/ubuntu/hw12/dummy/Module.symvers :=  scripts/mod/modpost -m -a      -o /home/ubuntu/hw12/dummy/Module.symvers -T /home/ubuntu/hw12/dummy/modules.order -i Module.symvers -e 
