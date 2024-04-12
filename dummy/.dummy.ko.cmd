b8f64a920407fcaf941ff4b9faf54bcd4d02d38c
Werapat Wangrungroj <94524977+hellp002@users.noreply.github.com>
2024-04-12 20:35:48 +0700

    Delete dummy/.dummy.ko.cmd

--- dummy/.dummy.ko.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/ubuntu/hw12/dummy/dummy.ko := ld -r -m elf_x86_64 -z noexecstack --build-id=sha1  -T scripts/module.lds -o /home/ubuntu/hw12/dummy/dummy.ko /home/ubuntu/hw12/dummy/dummy.o /home/ubuntu/hw12/dummy/dummy.mod.o;  make -f ./arch/x86/Makefile.postlink /home/ubuntu/hw12/dummy/dummy.ko
