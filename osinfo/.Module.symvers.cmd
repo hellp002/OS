04676e7e6b9c3c4944e0ae9add5e9b8dc73bbdba
Werapat Wangrungroj <94524977+hellp002@users.noreply.github.com>
2024-04-12 20:37:11 +0700

    Delete osinfo/.Module.symvers.cmd

--- osinfo/.Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/ubuntu/hw12/osinfo/Module.symvers :=  scripts/mod/modpost -m -a      -o /home/ubuntu/hw12/osinfo/Module.symvers -T /home/ubuntu/hw12/osinfo/modules.order -i Module.symvers -e 
