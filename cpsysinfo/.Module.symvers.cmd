f125dfd238dbc9e998b41f3544fe5ce0ab2156b9
Werapat Wangrungroj <94524977+hellp002@users.noreply.github.com>
2024-04-12 20:32:28 +0700

    Delete cpsysinfo/.Module.symvers.cmd

--- cpsysinfo/.Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/ubuntu/hw12/cpsysinfo/Module.symvers :=  scripts/mod/modpost -m -a      -o /home/ubuntu/hw12/cpsysinfo/Module.symvers -T /home/ubuntu/hw12/cpsysinfo/modules.order -i Module.symvers -e 
